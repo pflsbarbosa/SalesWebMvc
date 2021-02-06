e719867377fc88330788ee516c730ba9383d29d8
Paulo Barbosa <pflsbarbosa@gmail.com>
2021-02-06 16:36:45 +0000

    CRUD Scafolding

---
+++ SalesWebExerciseMvc/Data/SalesWebExerciseMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebExerciseMvc.Models;
+
+namespace SalesWebExerciseMvc.Data
+{
+    public class SalesWebExerciseMvcContext : DbContext
+    {
+        public SalesWebExerciseMvcContext (DbContextOptions<SalesWebExerciseMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebExerciseMvc.Models.Department> Department { get; set; }
+    }
+}
--- SalesWebExerciseMvc/Startup.cs
+++ SalesWebExerciseMvc/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebExerciseMvc.Data;
 
 namespace SalesWebExerciseMvc
 {
@@ -33,6 +35,9 @@ namespace SalesWebExerciseMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWebExerciseMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebExerciseMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
