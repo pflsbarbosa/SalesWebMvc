6f326b337e78c4ceeb180cd4969382d31368cc45
Paulo Barbosa <pflsbarbosa@gmail.com>
2021-03-23 15:58:03 +0000

    App locale, number and date formatting

--- SalesWebExerciseMvc/Models/Seller.cs
+++ SalesWebExerciseMvc/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebExerciseMvc.Models;
@@ -11,10 +12,21 @@ namespace SalesWebExerciseMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+        
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "{0:F2}€")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+
+        [Display(Name = "Department Id")]
         public int DepartmentId { get; set; } //EF see int as not null field in database
         public ICollection<SalesRecord> Sales { get; set; }
 
